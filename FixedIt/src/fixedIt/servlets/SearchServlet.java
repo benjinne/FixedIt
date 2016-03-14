5bb95615411b396fcc70e84f89d0fb62323570da
gghafir <gghafir@ycp.edu>
2016-03-14 19:43:47 -0400

    Added skeleton for SearchServlet. Included it in the jsp and xml files. Need to implement controller and finish up jsp and servlets

---
+++ FixedIt/src/fixedIt/servlets/SearchServlet.java
@@ -0,0 +1,47 @@
+package fixedIt.servlets;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fixedIt.modelComponents.Query;
+
+
+public class SearchServlet extends HttpServlet {			
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.getRequestDispatcher("/_view/userInfo.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		// Decode form parameters and dispatch to controller
+		//String errorMessage = null;
+		
+		
+		
+		
+		// Add parameters as request attributes
+	
+		
+		// Forward to view to render the result HTML document
+		req.getRequestDispatcher("/_view/search.jsp").forward(req, resp);
+	}
+
+	/*private String getStringFromParameter(String s) {
+		if (s == null || s.equals("")) {
+			return null;
+		} else {
+			return s;
+		}
+	}*/
+}
+
---
+++ FixedIt/war/_view/search.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+
+<html>
+	<head>
+		<title>Search for Classes:</title>
+	</head>
+
+	<body>
+		DO STUFF FOR SEARCH
+	</body>
+</html>
