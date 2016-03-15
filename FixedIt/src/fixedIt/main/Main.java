b6a0e3bd02413302f6b97afa3a3de71f4c821b39
Mat Jones <mjones44@ycp.edu>
2016-03-15 19:48:24 -0400

    moved initializeCourseTable() from Main to LoginServlet

--- FixedIt/src/fixedIt/main/Main.java
+++ FixedIt/src/fixedIt/main/Main.java
@@ -1,38 +1,14 @@
 package fixedIt.main;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-import fixedIt.modelComponents.Course;
-import fixedIt.modelComponents.Registrar;
-import fixedIt.sql.database.DBUtil;
-import fixedIt.sql.database.SQLWriter;
-
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-		Connection conn = null;
-		try {
-			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-			conn = DriverManager.getConnection("jdbc:derby:test.db;create=true");
-			conn.setAutoCommit(true);
-			initializeCoursesTable(conn);
-		} catch (SQLException e) {
-			System.out.println("Error: " + e.getMessage());
-		} finally {
-			DBUtil.closeQuietly(conn);
-		}
-		
-		
-		
+	public static void main(String[] args) throws Exception{		
 		Server server = new Server(8081);
 
 		// Create and register a webapp context
@@ -61,115 +37,5 @@ public class Main {
 		server.stop();
 		server.join();
 		System.out.println("Server has shut down, exiting");
-	}
-	
-	public static void initializeCoursesTable(Connection conn) throws SQLException, IOException{
-		ArrayList<String> depts=new ArrayList<String>();
-		addAllDepts(depts);
-		String sqlDelete="delete from courses where 1=1";
-		SQLWriter.executeDBCommand(conn, sqlDelete);
-		for(String s : depts){
-			Registrar r=new Registrar("http://ycpweb.ycp.edu/schedule-of-classes/index.html?term=201520" + "&stype=A&dmode=D&dept=" + s);
-			for (Course c : r.fetch()) {
-				String sql="insert into courses \n" +
-						"(CRN, courseAndSection, title, credits, type, days, time, location_one, location_two, instructor_one, instructor_two, capacity, seatsRemain, seatsFilled, beginEnd) \n" +
-						"values (\n'" +
-						c.getCRN() + "', \n'" + c.getCourseAndSection() + "', \n'" +
-						c.getTitle().replace("'", "''") + "', \n'" + c.getCredits() + "', \n'" + c.getType().replace("'", "''") + "', \n'" +
-						c.getDays() + "', \n'" + c.getTime() + "', \n";
-				if(c.getLocation().size()<2){
-					sql=sql+"'" + c.getLocation().get(0) + "', \n" + "'null', \n";
-				}
-				else{
-					sql=sql+"'" + c.getLocation().get(0) + "', \n'" + c.getLocation().get(1) + "', \n";
-				}
-				if(c.getInstructors().size()<2){
-					sql=sql+"'" + c.getInstructors().get(0).replace("'", "''") + "', \n" + "'null', \n";
-				}
-				else{
-					sql=sql+"'" + c.getInstructors().get(0).replace("'", "''") + "', \n'" + c.getInstructors().get(1).replace("'", "''") + "', \n";
-				}
-				sql=sql +"'" + c.getCapacity() + "', \n'" + c.getSeatsRemain() + "', \n'" + c.getSeatsFilled()
-					+ "', \n'" + c.getBeginEnd() + "'" + ")";
-				//System.out.println(sql);
-				SQLWriter.executeDBCommand(conn, sql);
-			}
-		}
-		//SQLWriter.executeSQL(conn, "select * from courses ");
-	}
-	
-//	CRN + ", " + courseAndSection + ", " + title + ", " + credits + ", " + type + ", " + days + ", " +
-//	time + ", " + locations + ", " + instructors + ", " + capacity + ", " + seatsRemain
-//	+ ", " + seatsFilled + ", " + beginEnd
-	
-	public static void addAllDepts(ArrayList<String> depts){
-		depts.add("ANT_01");
-		depts.add("BEH_01");
-		depts.add("CJA_01");
-		depts.add("GER_01");
-		depts.add("HSV_01");
-		depts.add("PSY_01");
-		depts.add("SOC_01");
-		depts.add("BIO_02");
-		depts.add("PMD_02");
-		depts.add("RT_02");
-		depts.add("ACC_03");
-		depts.add("ECO_03");
-		depts.add("ENT_03");
-		depts.add("FIN_03");
-		depts.add("BUS_03");
-		depts.add("IFS_03");
-		depts.add("IBS_03");
-		depts.add("MGT_03");
-		depts.add("MKT_03");
-		depts.add("QBA_03");
-		depts.add("SCM_03");
-		depts.add("ART_07");
-		depts.add("CM_07");
-		depts.add("MUS_07");
-		depts.add("THE_07");
-		depts.add("ECH_04");
-		depts.add("EDU_04");
-		depts.add("MLE_04");
-		depts.add("SE_04");
-		depts.add("SPE_04");
-		depts.add("CS_12");
-		depts.add("ECE_12");
-		depts.add("EGR_12");
-		depts.add("ME_12");
-		depts.add("PHY_12");
-		depts.add("FLM_05");
-		depts.add("FCO_05");
-		depts.add("FRN_05");
-		depts.add("GRM_05");
-		depts.add("HUM_05");
-		depts.add("INT_05");
-		depts.add("ITL_05");
-		depts.add("LAT_05");
-		depts.add("LIT_05");
-		depts.add("PHL_05");
-		depts.add("REL_05");
-		depts.add("RUS_05");
-		depts.add("SPN_05");
-		depts.add("WRT_05");
-		depts.add("G_06");
-		depts.add("HIS_06");
-		depts.add("IA_06");
-		depts.add("INT_06");
-		depts.add("PS_06");
-		depts.add("HSP_11");
-		depts.add("PE_11");
-		depts.add("REC_11");
-		depts.add("SPM_11");
-		depts.add("FYS_10");
-		depts.add("SES_10");
-		depts.add("WGS_10");
-		depts.add("NUR_08");
-		depts.add("CHM_09");
-		depts.add("ESS_09");
-		depts.add("FCM_09");
-		depts.add("MAT_09");
-		depts.add("PSC_09");
-		depts.add("PHY_09");
 	}
 }
--- FixedIt/src/fixedIt/servlets/LoginServlet.java
+++ FixedIt/src/fixedIt/servlets/LoginServlet.java
@@ -1,5 +1,9 @@
 package fixedIt.servlets;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import fixedIt.controllers.LoginController;
+import fixedIt.modelComponents.Course;
+import fixedIt.modelComponents.Registrar;
+import fixedIt.sql.database.DBUtil;
+import fixedIt.sql.database.SQLWriter;
 
 
 public class LoginServlet extends HttpServlet {
@@ -49,6 +57,126 @@ public class LoginServlet extends HttpServlet {
 		
 		// Forward to view to render the result HTML document
 		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+		try {
+			initializeCoursesTable();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void initializeCoursesTable() throws SQLException, IOException, ClassNotFoundException{
+		Connection conn = null;
+		try {
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+			conn = DriverManager.getConnection("jdbc:derby:test.db;create=true");
+			conn.setAutoCommit(true);
+		} catch (SQLException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			DBUtil.closeQuietly(conn);
+		}
+		
+		ArrayList<String> depts=new ArrayList<String>();
+		addAllDepts(depts);
+		String sqlDelete="delete from courses where 1=1";
+		SQLWriter.executeDBCommand(conn, sqlDelete);
+		for(String s : depts){
+			Registrar r=new Registrar("http://ycpweb.ycp.edu/schedule-of-classes/index.html?term=201520" + "&stype=A&dmode=D&dept=" + s);
+			for (Course c : r.fetch()) {
+				String sql="insert into courses \n" +
+						"(CRN, courseAndSection, title, credits, type, days, time, location_one, location_two, instructor_one, instructor_two, capacity, seatsRemain, seatsFilled, beginEnd) \n" +
+						"values (\n'" +
+						c.getCRN() + "', \n'" + c.getCourseAndSection() + "', \n'" +
+						c.getTitle().replace("'", "''") + "', \n'" + c.getCredits() + "', \n'" + c.getType().replace("'", "''") + "', \n'" +
+						c.getDays() + "', \n'" + c.getTime() + "', \n";
+				if(c.getLocation().size()<2){
+					sql=sql+"'" + c.getLocation().get(0) + "', \n" + "'null', \n";
+				}
+				else{
+					sql=sql+"'" + c.getLocation().get(0) + "', \n'" + c.getLocation().get(1) + "', \n";
+				}
+				if(c.getInstructors().size()<2){
+					sql=sql+"'" + c.getInstructors().get(0).replace("'", "''") + "', \n" + "'null', \n";
+				}
+				else{
+					sql=sql+"'" + c.getInstructors().get(0).replace("'", "''") + "', \n'" + c.getInstructors().get(1).replace("'", "''") + "', \n";
+				}
+				sql=sql +"'" + c.getCapacity() + "', \n'" + c.getSeatsRemain() + "', \n'" + c.getSeatsFilled()
+					+ "', \n'" + c.getBeginEnd() + "'" + ")";
+				SQLWriter.executeDBCommand(conn, sql);
+			}
+		}
+	}
+	
+	public static void addAllDepts(ArrayList<String> depts){
+		depts.add("ANT_01");
+		depts.add("BEH_01");
+		depts.add("CJA_01");
+		depts.add("GER_01");
+		depts.add("HSV_01");
+		depts.add("PSY_01");
+		depts.add("SOC_01");
+		depts.add("BIO_02");
+		depts.add("PMD_02");
+		depts.add("RT_02");
+		depts.add("ACC_03");
+		depts.add("ECO_03");
+		depts.add("ENT_03");
+		depts.add("FIN_03");
+		depts.add("BUS_03");
+		depts.add("IFS_03");
+		depts.add("IBS_03");
+		depts.add("MGT_03");
+		depts.add("MKT_03");
+		depts.add("QBA_03");
+		depts.add("SCM_03");
+		depts.add("ART_07");
+		depts.add("CM_07");
+		depts.add("MUS_07");
+		depts.add("THE_07");
+		depts.add("ECH_04");
+		depts.add("EDU_04");
+		depts.add("MLE_04");
+		depts.add("SE_04");
+		depts.add("SPE_04");
+		depts.add("CS_12");
+		depts.add("ECE_12");
+		depts.add("EGR_12");
+		depts.add("ME_12");
+		depts.add("PHY_12");
+		depts.add("FLM_05");
+		depts.add("FCO_05");
+		depts.add("FRN_05");
+		depts.add("GRM_05");
+		depts.add("HUM_05");
+		depts.add("INT_05");
+		depts.add("ITL_05");
+		depts.add("LAT_05");
+		depts.add("LIT_05");
+		depts.add("PHL_05");
+		depts.add("REL_05");
+		depts.add("RUS_05");
+		depts.add("SPN_05");
+		depts.add("WRT_05");
+		depts.add("G_06");
+		depts.add("HIS_06");
+		depts.add("IA_06");
+		depts.add("INT_06");
+		depts.add("PS_06");
+		depts.add("HSP_11");
+		depts.add("PE_11");
+		depts.add("REC_11");
+		depts.add("SPM_11");
+		depts.add("FYS_10");
+		depts.add("SES_10");
+		depts.add("WGS_10");
+		depts.add("NUR_08");
+		depts.add("CHM_09");
+		depts.add("ESS_09");
+		depts.add("FCM_09");
+		depts.add("MAT_09");
+		depts.add("PSC_09");
+		depts.add("PHY_09");
 	}
 
 	private String getStringFromParameter(String s) {
