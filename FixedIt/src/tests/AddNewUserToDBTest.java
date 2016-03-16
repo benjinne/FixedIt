d928bade40b084831c7cd2ddbdb256468eaaf0f8
Mat Jones <mjones44@ycp.edu>
2016-03-16 15:46:24 -0400

    added a test class for addNewUserToDB method

--- FixedIt/src/tests/AddNewUserToDBTest.java
+++ FixedIt/src/tests/AddNewUserToDBTest.java
@@ -1,9 +1,15 @@
 package tests;
 
+import fixedIt.modelComponents.*;
+
 public class AddNewUserToDBTest {
 
 	public static void main(String[] args) {
-		
+		Authenticator auth=new Authenticator();
+		String email="test@example.com";
+		String passHash=auth.saltHashPassword("password");
+		User user=new User(email, passHash, 0, 0, auth);
+		auth.addNewUserToDB(user);
 	}
 
 }
