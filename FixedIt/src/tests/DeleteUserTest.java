61e8b798958b5a7a8989886010e71736f63b2c86
Mat Jones <mjones44@ycp.edu>
2016-03-17 16:40:01 -0400

    added DeleteUserTest class

--- FixedIt/src/tests/DeleteUserTest.java
+++ FixedIt/src/tests/DeleteUserTest.java
@@ -9,6 +9,6 @@ public class DeleteUserTest {
 		String email="test@example.com";
 		String passHash=auth.saltHashPassword("password");
 		User user=new User(email, passHash, 0, 0, auth);
-		auth.addNewUserToDB(user);
+		auth.deleteUser(user);
 	}
 }
