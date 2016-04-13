5ff7487f301cd2d749d2fd135a1f35ec86b41c31
Mike Skurla <mskurla@ycp.edu>
2016-04-13 12:33:02 -0400

    i added querycontroller test to authenticator test

--- FixedIt/src/tests/AuthenticatorTest.java
+++ FixedIt/src/tests/AuthenticatorTest.java
@@ -5,13 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import fixedIt.controllers.QueryController;
 import fixedIt.modelComponents.Authenticator;
+import fixedIt.modelComponents.Course;
+import fixedIt.modelComponents.Query;
 
 public class AuthenticatorTest {
 	private Authenticator a;
 	private String validPass;
 	private String validPass2;
 	private String invalidPass;
+	private QueryController q;
+	//private String search1;
+	//private String search2;
+	
+	
 	
 	@Before
 	public void setUp(){
@@ -62,8 +70,23 @@ public class AuthenticatorTest {
 	public void testValidatePassword(){
 		String hash1=a.saltHashPassword(validPass);
 		String hash2=a.saltHashPassword(validPass2);
-		
+	
 		assertTrue(a.credentialsMatch(validPass, hash1));
 		assertTrue(a.credentialsMatch(validPass2, hash2));
 	}
+	@Test
+	public void testQueryController(){
+		String search1="ANT201.106";
+		String search2="PE102.50";
+		String search3="PE125.802";
+		
+		
+		assertTrue(q.getQuery().equals(search1));
+		assertTrue(q.getQuery().equals(search3));
+		assertFalse(q.getQuery().equals(search2));
+		
+		
+	}
+
+	
 }
