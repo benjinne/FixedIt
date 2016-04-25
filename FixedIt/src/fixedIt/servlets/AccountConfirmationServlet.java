3bd4dd961bd2c50e2d02f9e69955c1d08ba821f4
Mat Jones <mjones44@ycp.edu>
2016-04-25 12:55:42 -0400

    created AccountConfirmationServlet, added servlet mapping

---
+++ FixedIt/src/fixedIt/servlets/AccountConfirmationServlet.java
@@ -0,0 +1,17 @@
+package fixedIt.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AccountConfirmationServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+		
+	}
+}
